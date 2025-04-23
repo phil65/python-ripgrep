607253a89a58bd6827b82708f91eb90991fadf1b
Johann Miller <22018973+johannkm@users.noreply.github.com>
2025-04-23 13:19:04 -0400

    update ruff bc morph decided we should (#2940)

--- python_ripgrep/__init__.py
+++ python_ripgrep/__init__.py
@@ -1,3 +1,3 @@
 from .python_ripgrep import PySortMode, PySortModeKind, files, search
 
-__all__ = ["search", "files", "PySortMode", "PySortModeKind"]
+__all__ = ["PySortMode", "PySortModeKind", "files", "search"]
